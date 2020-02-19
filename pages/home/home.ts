094c5849c67e387317062f30b5f7d8a584e97620
kaiqeduardo <kaiqeduardo@hotmail.com>
2020-02-19 17:12:59 +0000

    3 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -6,9 +6,16 @@ import { NavController } from "ionic-angular";
   templateUrl: "home.html"
 })
 export class HomePage {
-  
+  resultView: Array<{ i: number; j: number; letter: string }> = [];
+
   inputValue = "";
-  matrix = [["a", "b", "c"], ["a", "e", "f"], ["g", "h", "i"]];
+  matrix = [
+    ["a", "b", "c", "d", "e", "f"],
+    ["g", "h", "c", "i", "j", "k"],
+    ["l", "m", "n", "a", "o", "p"],
+    ["q", "r", "s", "t", "s", "u"],
+    ["v", "w", "x", "y", "z", "a"]
+  ];
   positions = [
     "left",
     "rigth",
@@ -28,29 +35,62 @@ export class HomePage {
   constructor(public navCtrl: NavController) {}
 
   checkIfExist() {
+    this.resultView = [];
+    this.firstLetterPosition = [];
+
     let splitedInputValue: Array<string> = this.inputValue.split("");
     this.maxIValue = this.matrix.length;
     this.maxJValue = this.matrix[this.matrix.length - 1].length;
-    console.log(`maxIValue ${this.maxIValue} - maxJValue ${this.maxJValue}`);
-    console.log(splitedInputValue);
+    // console.log(`maxIValue ${this.maxIValue} - maxJValue ${this.maxJValue}`);
+    // console.log(splitedInputValue);
     if (splitedInputValue.length > 0) {
       this.findFirstLetter(splitedInputValue[0]);
-      console.log("aux", this.firstLetterPosition);
+      // console.log("aux", this.firstLetterPosition);
       if (splitedInputValue.length > 1) {
         // searching for the actual word
         this.firstLetterPosition.forEach(data => {
           let secondPosition = [];
-
           this.positions.forEach(p => {
             let x = this.positionNextLetter(data.i, data.j, p);
-            console.log('x',x)
-            if (x && (splitedInputValue[1] === this.matrix[x.i][x.j])) {
+            if (x && splitedInputValue[1] === this.matrix[x.i][x.j]) {
               x.letter = splitedInputValue[1];
               secondPosition.push(x);
+              this.mountWord(data.i, data.j, splitedInputValue[0]);
+              this.mountWord(x.i, x.j, x.letter);
             }
           });
 
-          console.log("seconPosition", secondPosition);
+          if (splitedInputValue.length > 2) {
+            let copySecondPosition = Object.assign([], secondPosition);
+
+            copySecondPosition.forEach(s => {
+              let position: number = 2;
+              let k = s;
+              do {
+                let x = this.positionNextLetter(k.i, k.j, s.direction);
+
+                if (
+                  x &&
+                  splitedInputValue[position] === this.matrix[x.i][x.j]
+                ) {
+                  x.letter = splitedInputValue[position];
+                  this.mountWord(x.i, x.j, x.letter);
+                  k = x;
+                } else {
+                  break;
+                }
+
+                position++;
+              } while (position < splitedInputValue.length);
+            });
+          }
+
+          // console.log(" I ",data.i," J ",data.j, this.matrix[data.i][data.j]);
+          // console.log("Second", secondPosition);
+          // secondPosition.forEach(s => {
+          //  console.log(" I ", s.i, " J ", s.j, this.matrix[s.i][s.j]);
+          // });
+          console.log(" Result ", this.resultView);
         });
       }
     } else {
@@ -63,8 +103,8 @@ export class HomePage {
       this.matrix[i].forEach((itemJ, j) => {
         if (letter === this.matrix[i][j]) {
           this.firstLetterPosition.push({ i, j });
-          console.log(this.firstLetterPosition);
-          console.log(`i ${i} - j ${j}`);
+          // console.log(this.firstLetterPosition);
+          // console.log(`i ${i} - j ${j}`);
         }
       });
     });
@@ -77,61 +117,71 @@ export class HomePage {
   ): { i: number; j: number; direction: string; letter: string } {
     switch (direction) {
       case "left": {
-        if (j-- >= this.minJValue) {
-          return { i, j: j--, direction, letter: "" };
+        if (j - 1 >= this.minJValue) {
+          return { i, j: j - 1, direction, letter: "" };
         } else {
           return null;
         }
       }
       case "rigth": {
-        if (j++ <= this.maxJValue) {
-          return { i, j: j++, direction, letter: "" };
+        if (j + 1 <= this.maxJValue) {
+          return { i, j: j + 1, direction, letter: "" };
         } else {
           return null;
         }
       }
       case "top": {
-        if (i-- >= this.minIValue) {
-          return { i: i--, j, direction, letter: "" };
+        if (i - 1 >= this.minIValue) {
+          return { i: i - 1, j, direction, letter: "" };
         } else {
           return null;
         }
       }
       case "bottom": {
-        if (i++ <= this.maxIValue) {
-          return { i: i++, j, direction, letter: "" };
+        if (i + 1 < this.maxIValue) {
+          return { i: i + 1, j, direction, letter: "" };
         } else {
           return null;
         }
       }
       case "leftTop": {
-        if (i-- >= this.minIValue && j-- >= this.minJValue) {
-          return { i: i--, j: j--, direction, letter: "" };
+        if (i - 1 >= this.minIValue && j - 1 >= this.minJValue) {
+          return { i: i - 1, j: j - 1, direction, letter: "" };
         } else {
           return null;
         }
       }
       case "leftBottom": {
-        if (i++ <= this.maxIValue && j-- >= this.minJValue) {
-          return { i: i++, j: j--, direction, letter: "" };
+        if (i + 1 < this.maxIValue && j - 1 >= this.minJValue) {
+          return { i: i + 1, j: j - 1, direction, letter: "" };
         } else {
           return null;
         }
       }
       case "rigthTop": {
-        if (i-- >= this.minIValue && j++ <= this.maxJValue) {
-          return { i: i--, j: j++, direction, letter: "" };
+        if (i - 1 >= this.minIValue && j + 1 < this.maxJValue) {
+          return { i: i - 1, j: j + 1, direction, letter: "" };
         } else {
           return null;
         }
       }
       case "rigthBottom": {
-        if (i++ <= this.maxIValue && j++ <= this.maxJValue) {
-          return { i: i++, j: j++, direction, letter: "" };
+        if (i + 1 < this.maxIValue && j + 1 < this.maxJValue) {
+          return { i: i + 1, j: j + 1, direction, letter: "" };
         } else {
           return null;
         }
       }
     }
+  }
+
+  mountWord(i: number, j: number, letter: string) {
+    this.resultView.push({ i, j, letter });
+  }
+
+  existLetter(i: number, j: number): boolean {
+    let aux = this.resultView.findIndex(dt => dt.i === i && dt.j === j);
+    if (aux !== -1) return true;
+    else return false;
   }
 }
